d8df865b2a5ff554798d3624930283e1171fdce0
Lukas Schreiner <lukas.schreiner@fls-wiesbaden.de>
2016-02-29 21:51:36 +0100

    Added the license information to all sources.

--- standin/views.py
+++ standin/views.py
@@ -1,5 +1,21 @@
 # -*- coding: utf-8 -*-
 # vim: fenc=utf-8:ts=8:sw=8:si:sta:noet
+#
+# Standin plan as extension to Django framework.
+# Copyright (C) 2016 Friedrich-List-Schule Wiesbaden
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.shortcuts import render
 
